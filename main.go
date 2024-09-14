235fb17372158036ed9bace7e9f8e8ea2c3fb199
Rostislav <rostislav.solomakho@gmail.com>
2024-09-14 16:24:43 +0300

    В ветке branch1 -> Удалил выводы

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Тестовый проект!")
-	fmt.Println("Тестовый проект!")
+	fmt.Println("1")
 
-	fmt.Println("Работа в новой ветке")
-	fmt.Println("Добавил вывод в ветке branch1")
 }
